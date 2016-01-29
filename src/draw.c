f8393b8cb01f5f6bb2073539ea841d5a02d8efed
id101010 <aaron@duckpond.ch>
2016-01-29 15:36:51 +0100

    Bugfix

--- src/draw.c
+++ src/draw.c
@@ -1,5 +1,9 @@
-#include<stdlib.c>
-#include<stdint.c>
+#include<stdint.h>
+#include"ssd1963.h"
+#include"ssd1963_lld.h"
+#include"ssd1963_cmd.h"
+#include"lcd_conf.h"
+#include"draw.h"
 
 #define BYTES_PER_PIXEL 2
 
@@ -8,14 +12,14 @@ uint16_t bitmap_draw(   unsigned int width, unsigned int height,
                         unsigned char *pixel_data){
     
     if(bytes_per_pixel != BYTES_PER_PIXEL){
-        return -1
+        return -1;
     }
 
     SSD1963_SetArea(0, 0, width - 1, height - 1);
     SSD1963_WriteCommand(CMD_WR_MEMSTART);
 
     for(int i = 0; i < width * height; i++){
-        SSD193_WriteData(((uint16_t*)pixel_data)[i]);
+        SSD1963_WriteData(((uint16_t*)pixel_data)[i]);
     }
 
     return 0;
